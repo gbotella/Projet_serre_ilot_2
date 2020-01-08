763c57927831daf73e695d7253dec6fa66e17c39
ielyattaouimalek <ielyattaouimalek@btslivh.eu>
2020-01-08 15:37:40 +0100

    declaration classe

---
+++ VueSerreTesteur_2/InfoClimat_2.cpp
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   InfoClimat.cpp
+ * Author: snir2g1
+ * 
+ * Created on 8 janvier 2020, 15:04
+ */
+
+#include "InfoClimat_2.h"
+
+InfoClimat::InfoClimat() {
+}
+
+InfoClimat::InfoClimat(const InfoClimat& orig) {
+}
+
+InfoClimat::~InfoClimat() {
+}
+
+int InfoClimat::hygrometrie() {
+}
+
+int InfoClimat::temperature() {
+}
+
+int InfoClimat::positionOuvrants() {
+}
+
+int InfoClimat::luminosite() {
+}
+
+int InfoClimat::date() {
+}
